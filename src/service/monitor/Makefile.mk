e750c080c38ba9859ed948b3f7ffb46a8e9414a1
bubble <bubble@dipcc.com.cn>
2015-02-13 14:22:38 +0800

    将监控模块的Makefile提交上去

---
+++ src/service/monitor/Makefile.mk
@@ -0,0 +1,43 @@
+CFILE += $(SROUCE)/service/monitor/mon_lib.c \
+	$(SROUCE)/service/monitor/mon_get_cpu_info.c \
+	$(SROUCE)/service/monitor/mon_get_mem_info.c \
+	$(SROUCE)/service/monitor/mon_get_disk_info.c \
+	$(SROUCE)/service/monitor/mon_get_net_info.c \
+	$(SROUCE)/service/monitor/mon_get_proc_info.c \
+	$(SROUCE)/service/monitor/mon_monitor_and_handle.c \
+	$(SROUCE)/service/monitor/mon_notification.c \
+	$(SROUCE)/service/monitor/mon_warning_msg_queue.c \
+	$(SROUCE)/service/monitor/mon_pub.c
+	
+C_OBJ_FILE += mon_lib.$(SUFFIX) \
+	mon_get_cpu_info.$(SUFFIX) \
+	mon_get_mem_info.$(SUFFIX) \
+	mon_get_disk_info.$(SUFFIX) \
+	mon_get_net_info.$(SUFFIX) \
+	mon_get_proc_info.$(SUFFIX) \
+	mon_monitor_and_handle.$(SUFFIX) \
+	mon_notification.$(SUFFIX) \
+	mon_warning_msg_queue.$(SUFFIX) \
+	mon_pub.$(SUFFIX)
+	
+mon_lib.$(SUFFIX):
+	$(C_COMPILE) $(SROUCE)/service/monitor/mon_lib.c
+mon_get_cpu_info.$(SUFFIX):
+	$(C_COMPILE) $(SROUCE)/service/monitor/mon_get_cpu_info.c
+mon_get_mem_info.$(SUFFIX):
+	$(C_COMPILE) $(SROUCE)/service/monitor/mon_get_mem_info.c
+mon_get_disk_info.$(SUFFIX):
+	$(C_COMPILE) $(SROUCE)/service/monitor/mon_get_disk_info.c
+mon_get_net_info.$(SUFFIX):
+	$(C_COMPILE) $(SROUCE)/service/monitor/mon_get_net_info.c
+mon_get_proc_info.$(SUFFIX):
+	$(C_COMPILE) $(SROUCE)/service/monitor/mon_get_proc_info.c
+mon_monitor_and_handle.$(SUFFIX):
+	$(C_COMPILE) $(SROUCE)/service/monitor/mon_monitor_and_handle.c
+mon_notification.$(SUFFIX):
+	$(C_COMPILE) $(SROUCE)/service/monitor/mon_notification.c
+mon_warning_msg_queue.$(SUFFIX):
+	$(C_COMPILE) $(SROUCE)/service/monitor/mon_warning_msg_queue.c
+mon_pub.$(SUFFIX):
+	$(C_COMPILE) $(SROUCE)/service/monitor/mon_pub.c
+	
