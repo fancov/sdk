119e2ca7b2554ff019ea3205dbaffb397641802a
larry <larry@dipcc.com>
2015-03-30 09:09:49 +0800

    添加编解码工具相关文件
    Signed-off-by: larry <larry@dipcc.com>

---
+++ scripts/codec/codec_convert.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+CODEC_APP="./codec_convert"
+SOX_APP="sox"
+
+print_usage()
+{
+    echo "    ERROR: "$1
+    echo ""
+    echo "    Use this command as: codec_convert decodec|encodec path <input file> <output file>"
+    echo ""
+}
+
+if [ $# -lt 3 ]; then
+    print_usage
+    exit 255
+fi
+
+if [ ! -f $CODEC_APP ]; then
+	echo "Cannot find the codec_convert cmd."
+	exit 2
+fi
+
+FILE_PATH=$2
+
+INPUT_FILE="$FILE_PATH/$3"
+if [ ! -f $INPUT_FILE ]; then
+    exit 2
+fi
+
+if [ -z $4 ]; then
+    OUTPUT_FILE=${INPUT_FILE%.*}
+else
+    OUTPUT_FILE=$4
+fi
+
+case $1 in
+    encodec)
+        $SOX_APP $INPUT_FILE -c 1 -e u-law -r 8000 -t raw $OUTPUT_FILE.PCMU
+        if [ $? -ne 0 ]; then
+            exit 1
+        fi
+
+        $SOX_APP $INPUT_FILE  -c 1 -e a-law -r 8000 -t raw $OUTPUT_FILE.PCMA
+        if [ $? -ne 0 ]; then
+            exit 1
+        fi
+
+        $CODEC_APP -ic PCMA -if $INPUT_FILE -oc G729 -of $OUTPUT_FILE.G729
+        if [ $? -ne 0 ]; then
+            exit 1
+        fi
+    ;;
+    decodec)
+        FILE_TYPE=${INPUT_FILE##*.}
+        case $FILE_TYPE in
+            PCMA)
+                $SOX_APP -e a-law -r 8000 -t raw $INPUT_FILE $OUTPUT_FILE.wav
+                if [ $? != 0 ]; then
+                    exit 1
+                fi
+            ;;
+            PCMU)
+                $SOX_APP -e u-law -r 8000 -t raw $INPUT_FILE $OUTPUT_FILE.wav
+                if [ $? -ne 0 ]; then
+                    exit 1
+                fi
+            ;;
+            G729)
+                $CODEC_APP -ic G729 -if $INPUT_FILE -oc PCMU -of $OUTPUT_FILE.tmp.PCMU
+                if [ $? -ne 0 ]; then
+                    exit 1
+                fi
+            
+                $SOX_APP -e u-law -r 8000 -t raw $4.tmp.PCMU $OUTPUT_FILE.wav
+                if [ $? -ne 0 ]; then
+                    exit 1
+                fi
+                
+                rm -rf $OUTPUT_FILE.tmp.PCMU
+            ;;
+            *)
+                print_usage
+                exit 255
+            ;;
+        esac        
+    ;;
+    *)
+    print_usage
+    exit 255
+esac
+
+exit 0
