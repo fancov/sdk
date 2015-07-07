a2831f9166a6962745a05282626a30a602b0f062
bubble <bubble@dipcc.com.cn>
2015-07-07 11:50:01 +0800

    按照代码规范修改完监控模块

--- src/service/monitor/mon_get_mem_info.c
+++ src/service/monitor/mon_get_mem_info.c
@@ -4,6 +4,7 @@ extern "C"{
 
 
 #include <dos.h>
+#include <dos/dos_mem.h>
 
 #if (INCLUDE_BH_SERVER)
 #if INCLUDE_RES_MONITOR
@@ -11,6 +12,7 @@ extern "C"{
 
 #include "mon_get_mem_info.h"
 #include "mon_lib.h"
+#include "mon_def.h"
 
 static S8  m_szMemInfoFile[] = "/proc/meminfo";
 extern S8  g_szMonMemInfo[MAX_BUFF_LENGTH];
@@ -28,17 +30,16 @@ static U32  mon_mem_reset_data();
  */
 U32  mon_mem_malloc()
 {
-   g_pstMem = (MON_SYS_MEM_DATA_S *)dos_dmem_alloc(sizeof(MON_SYS_MEM_DATA_S));
-   if (DOS_ADDR_INVALID(g_pstMem))
-   {
-      logr_cirt("%s:Line %u: mon_mem_malloc|pstMem is %p!",
-                dos_get_filename(__FILE__), __LINE__, g_pstMem);
-      return DOS_FAIL;
-   }
+    g_pstMem = (MON_SYS_MEM_DATA_S *)dos_dmem_alloc(sizeof(MON_SYS_MEM_DATA_S));
+    if (DOS_ADDR_INVALID(g_pstMem))
+    {
+        DOS_ASSERT(0);
+        return DOS_FAIL;
+    }
 
-   memset(g_pstMem, 0, sizeof(MON_SYS_MEM_DATA_S));
+    dos_memzero(g_pstMem, sizeof(MON_SYS_MEM_DATA_S));
 
-   return DOS_SUCC;
+    return DOS_SUCC;
 }
 
 
@@ -51,17 +52,16 @@ U32  mon_mem_malloc()
  */
 U32 mon_mem_free()
 {
-   if (DOS_ADDR_INVALID(g_pstMem))
-   {
-      logr_notice("%s:line %u: mon_mem_free|free memory failure,g_pstMem is %p!"
-                    , dos_get_filename(__FILE__), __LINE__, g_pstMem);
-      return DOS_FAIL;
-   }
+    if (DOS_ADDR_INVALID(g_pstMem))
+    {
+        DOS_ASSERT(0);
+        return DOS_FAIL;
+    }
 
-   dos_dmem_free(g_pstMem);
-   g_pstMem = NULL;
+    dos_dmem_free(g_pstMem);
+    g_pstMem = NULL;
 
-   return DOS_SUCC;
+    return DOS_SUCC;
 }
 
 
@@ -100,103 +100,99 @@ static U32  mon_mem_reset_data()
  */
 U32  mon_read_mem_file()
 {
-   U32 ulCount = 0;
-   S8  szLine[MAX_BUFF_LENGTH] = {0};
-   S8* pszAnalyseRslt[3] = {0};
-   U32 ulRet = 0;
+    U32 ulCount = 0;
+    S8  szLine[MAX_BUFF_LENGTH] = {0};
+    S8* pszAnalyseRslt[3] = {0};
+    U32 ulRet = 0;
 
-   FILE * fp = fopen(m_szMemInfoFile, "r");
-   if (DOS_ADDR_INVALID(fp))
-   {
-      logr_cirt("%s:line %u: mon_read_mem_file |file \'%s\' fp is %p!"
-                , dos_get_filename(__FILE__), __LINE__, m_szMemInfoFile, fp);
-      goto failure;
-   }
+    FILE * fp = fopen(m_szMemInfoFile, "r");
+    if (DOS_ADDR_INVALID(fp))
+    {
+        DOS_ASSERT(0);
+        goto failure;
+    }
 
-   fseek(fp, 0, SEEK_SET);
-   mon_mem_reset_data();
+    fseek(fp, 0, SEEK_SET);
+    mon_mem_reset_data();
 
-   while (!feof(fp))
-   {
-      if ( NULL != (fgets(szLine, MAX_BUFF_LENGTH, fp)))
-      {
-         S32 lRet = 0;
-         U32 ulData = 0;
-         ulRet = mon_analyse_by_reg_expr(szLine, " \t\n", pszAnalyseRslt, sizeof(pszAnalyseRslt)/sizeof(pszAnalyseRslt[0]));
-         if(DOS_SUCC != ulRet)
-         {
-            logr_error("%s:Line %u:mon_read_mem_file|analyse failure!"
-                        , dos_get_filename(__FILE__), __LINE__);
-            goto failure;
-         }
-
-         lRet = dos_atoul(pszAnalyseRslt[1], &ulData);
-         if(0 > lRet)
-         {
-            logr_error("%s:Line %u:mon_read_mem_file|dos_atol failure!"
-                        , dos_get_filename(__FILE__), __LINE__);
-            goto failure;
-         }
-         /* 第一列是内容，第二列为其值，以此类推 */
-         if (0 == dos_strnicmp(pszAnalyseRslt[0], "MemTotal", dos_strlen("MemTotal")))
-         {
-            g_pstMem->ulPhysicalMemTotalBytes = ulData;
-            ++ulCount;
-            continue;
-         }
-         else if(0 == dos_strnicmp(pszAnalyseRslt[0], "MemFree", dos_strlen("MemFree")))
-         {
-            g_pstMem->ulPhysicalMemFreeBytes = ulData;
-            ++ulCount;
-            continue;
-         }
-
-         else if(0 == dos_strnicmp(pszAnalyseRslt[0], "SwapTotal", dos_strlen("SwapTotal")))
-         {
-            g_pstMem->ulSwapTotalBytes = ulData;
-            ++ulCount;
-            continue;
-         }
-         else if(0 == dos_strnicmp(pszAnalyseRslt[0], "SwapFree", dos_strlen("SwapFree")))
-         {
-            g_pstMem->ulSwapFreeBytes = ulData;
-            ++ulCount;
-            continue;
-         }
-         else if(0 == dos_strnicmp(pszAnalyseRslt[0], "Buffers", dos_strlen("Buffers")))
-         {
-            g_pstMem->ulBuffers = ulData;
-            ++ulCount;
-            continue;
-         }
-         else if (0 == dos_strnicmp(pszAnalyseRslt[0], "Cached", dos_strlen("Cached")))
-         {
-            g_pstMem->ulCached = ulData;
-            ++ulCount;
-            continue;
-         }
-
-         if (MEMBER_COUNT == ulCount)
-         {
-            ulRet = mon_get_mem_data();
+    while (!feof(fp))
+    {
+        if ( NULL != (fgets(szLine, MAX_BUFF_LENGTH, fp)))
+        {
+            S32 lRet = 0;
+            U32 ulData = 0;
+            ulRet = mon_analyse_by_reg_expr(szLine, " \t\n", pszAnalyseRslt, sizeof(pszAnalyseRslt)/sizeof(pszAnalyseRslt[0]));
             if(DOS_SUCC != ulRet)
             {
-               logr_error("%s:Line %u:mon_read_mem_file|get memory data failure"
-                           , dos_get_filename(__FILE__), __LINE__);
+                DOS_ASSERT(0);
+                goto failure;
             }
-            goto success;
-         }
-      }
-   }
+
+            lRet = dos_atoul(pszAnalyseRslt[1], &ulData);
+            if(0 > lRet)
+            {
+                DOS_ASSERT(0);
+                goto failure;
+            }
+            /* 第一列是内容，第二列为其值，以此类推 */
+            if (0 == dos_strnicmp(pszAnalyseRslt[0], "MemTotal", dos_strlen("MemTotal")))
+            {
+                g_pstMem->ulPhysicalMemTotalBytes = ulData;
+                ++ulCount;
+                continue;
+            }
+            else if(0 == dos_strnicmp(pszAnalyseRslt[0], "MemFree", dos_strlen("MemFree")))
+            {
+                g_pstMem->ulPhysicalMemFreeBytes = ulData;
+                ++ulCount;
+                continue;
+            }
+
+            else if(0 == dos_strnicmp(pszAnalyseRslt[0], "SwapTotal", dos_strlen("SwapTotal")))
+            {
+                g_pstMem->ulSwapTotalBytes = ulData;
+                ++ulCount;
+                continue;
+            }
+            else if(0 == dos_strnicmp(pszAnalyseRslt[0], "SwapFree", dos_strlen("SwapFree")))
+            {
+                g_pstMem->ulSwapFreeBytes = ulData;
+                ++ulCount;
+                continue;
+            }
+            else if(0 == dos_strnicmp(pszAnalyseRslt[0], "Buffers", dos_strlen("Buffers")))
+            {
+                g_pstMem->ulBuffers = ulData;
+                ++ulCount;
+                continue;
+            }
+            else if (0 == dos_strnicmp(pszAnalyseRslt[0], "Cached", dos_strlen("Cached")))
+            {
+                g_pstMem->ulCached = ulData;
+                ++ulCount;
+                continue;
+            }
+
+            if (MEMBER_COUNT == ulCount)
+            {
+                ulRet = mon_get_mem_data();
+                if(DOS_SUCC != ulRet)
+                {
+                    mon_trace(MON_TRACE_MEM, LOG_LEVEL_ERROR, "Get Memory Info FAIL.");
+                }
+                goto success;
+            }
+        }
+    }
 
 success:
-   fclose(fp);
-   fp = NULL;
-   return DOS_SUCC;
+    fclose(fp);
+    fp = NULL;
+    return DOS_SUCC;
 failure:
-   fclose(fp);
-   fp = NULL;
-   return DOS_FAIL;
+    fclose(fp);
+    fp = NULL;
+    return DOS_FAIL;
 }
 
 /**
@@ -208,42 +204,42 @@ failure:
  */
 U32  mon_get_mem_data()
 {
-   U32 ulPhyMemTotal = g_pstMem->ulPhysicalMemTotalBytes;
-   U32 ulPhyMemFree  = g_pstMem->ulPhysicalMemFreeBytes;
+    U32 ulPhyMemTotal = g_pstMem->ulPhysicalMemTotalBytes;
+    U32 ulPhyMemFree  = g_pstMem->ulPhysicalMemFreeBytes;
 
-   U32 ulBusyMem = ulPhyMemTotal - ulPhyMemFree;
+    U32 ulBusyMem = ulPhyMemTotal - ulPhyMemFree;
 
-   U32 ulSwapTotal   = g_pstMem->ulSwapTotalBytes;
-   U32 ulSwapFree    = g_pstMem->ulSwapFreeBytes;
+    U32 ulSwapTotal   = g_pstMem->ulSwapTotalBytes;
+    U32 ulSwapFree    = g_pstMem->ulSwapFreeBytes;
 
-   U32 ulBusySwap = ulSwapTotal - ulSwapFree;
+    U32 ulBusySwap = ulSwapTotal - ulSwapFree;
 
-   /**
+    /**
     * 内存占用率算法:
     *     如果高速缓存的大小大于RAM大小，那么被占用的就是: busy = total -free
     *     否则被占用的为: busy = total - free - buffers - cached
     */
-   if (0 == ulPhyMemTotal)
-   {
-       g_pstMem->ulPhysicalMemUsageRate = 0;
-   }
-   else
-   {
-       ulBusyMem *= 100;
-       g_pstMem->ulPhysicalMemUsageRate = (ulBusyMem + ulBusyMem % ulPhyMemTotal) / ulPhyMemTotal;
-   }
+    if (0 == ulPhyMemTotal)
+    {
+        g_pstMem->ulPhysicalMemUsageRate = 0;
+    }
+    else
+    {
+        ulBusyMem *= 100;
+        g_pstMem->ulPhysicalMemUsageRate = (ulBusyMem + ulBusyMem % ulPhyMemTotal) / ulPhyMemTotal;
+    }
 
-   if (0 == ulSwapTotal)
-   {
-       g_pstMem->ulSwapUsageRate = 0;
-   }
-   else
-   {
-       ulSwapFree *= 100;
-       g_pstMem->ulSwapUsageRate = (ulBusySwap + ulBusySwap % ulSwapTotal) / ulSwapTotal;
-   }
+    if (0 == ulSwapTotal)
+    {
+        g_pstMem->ulSwapUsageRate = 0;
+    }
+    else
+    {
+        ulSwapFree *= 100;
+        g_pstMem->ulSwapUsageRate = (ulBusySwap + ulBusySwap % ulSwapTotal) / ulSwapTotal;
+    }
 
-   return DOS_SUCC;
+    return DOS_SUCC;
 }
 
 #endif //end #if INCLUDE_MEM_MONITOR
--- src/service/monitor/mon_get_net_info.h
+++ src/service/monitor/mon_get_net_info.h
@@ -23,12 +23,12 @@
 
 typedef struct tagNetcardParam //网卡信息
 {
-   S8  szNetDevName[32];    //网络设备名称
-   S8  szMacAddress[32];    //设备MAC地址
-   S8  szIPAddress[32];     //本地IPv4地址
-   S8  szBroadIPAddress[32];//广播IP地址
-   S8  szNetMask[32];       //子网掩码
-   U32 ulRWSpeed;            //数据传输最大速率，单位KB/s
+    S8  szNetDevName[32];    //网络设备名称
+    S8  szMacAddress[32];    //设备MAC地址
+    S8  szIPAddress[32];     //本地IPv4地址
+    S8  szBroadIPAddress[32];//广播IP地址
+    S8  szNetMask[32];       //子网掩码
+    U32 ulRWSpeed;            //数据传输最大速率，单位KB/s
 }MON_NET_CARD_PARAM_S;
 
 
