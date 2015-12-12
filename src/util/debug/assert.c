702e1134f93d9bc9078a351ccaf946f1db4707eb
larry <larry@dipcc.com>
2015-12-12 12:46:42 +0800

    断言函数回滚
    Signed-off-by: larry <larry@dipcc.com>

--- src/util/debug/assert.c
+++ src/util/debug/assert.c
@@ -264,15 +264,14 @@ S32 dos_assert_record()
  *      const S8 *pszFileName, const U32 ulLine, const U32 param ： 断言描述信息
  * 返回值: VOID
  */
-DLLEXPORT VOID dos_assert(const S8 *pszFunctionName, const S8 *pszFileName, const U32 ulLine, const U32 param)
+DLLEXPORT VOID dos_assert(const S8 *pszFileName, const U32 ulLine, const U32 param)
 {
     U32 ulHashIndex;
     S8 szFileLine[256] = { 0, };
     DOS_ASSERT_NODE_ST *pstFileDescNode = NULL;
 
 #ifdef INCLUDE_CC_SC
-    sc_log_digest_print("Assert happened: func:%s,file=%s,line=%d, param:%d"
-        , pszFunctionName, pszFileName, ulLine , param);
+    sc_log_digest_print("Assert happened: file=%s, line=%d, param:%d", pszFileName, ulLine , param);
 #endif
 
     /* 生产hash表key */
